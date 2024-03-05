32a5d44640da075123828ac0d3c9c5b91abf6636
Ludvig Alvir <ludvig.alvir@gmail.com>
2024-03-05 14:36:25 +0100

    fix

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -2,23 +2,21 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 export default function Info() {
 	const { id } = useParams();
-	const arr = useState(null);
-	cosnt[(data, setData)] = useState();
+	const [data, setData] = useState();
 
-	//data = arr[0], setData = arr[1]
 	useEffect(() => {
 		async function fetchData() {
 			const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
 			const data = await res.json();
 			console.log(data);
-			arr[1](data);
+			setData(data);
 		}
 		fetchData();
 	}, [id]);
 	return (
 		<div className="fixed">
 			<h2>Pokemon ID: {id}</h2>
-			{arr[0] ? <img src={arr[0].sprites.front_shiny} /> : ""}
+			{data ? <img src={data.sprites.front_shiny} /> : ""}
 		</div>
 	);
 }
